bb0f20c04c5356d64a63f0ca492c14f5b09318ed
Sercan Barış <sercanbaris@gmail.com>
2024-10-14 21:59:39 +0300

    Update deployment configuration for cPanel

--- src/pages/Deneme.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Deneme() {
-  return (
-    <div>Deneme asl</div>
-  )
-}
-
-export default Deneme
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,12 +3,38 @@ import "../css/services.css";
 
 function Services() {
   return (
-    <div>
-      {" "}
-      <img
+    <div className="service-container">
+      <h1 className="service-title">Services</h1>
+      <div className="services">
+        <div className="services-lane">
+          <div className="level-container">
+            <div className="level">A1</div>
+          </div>
+          <div className="level-container">
+            <div className="level">A2</div>
+          </div>
+          <div className="level-container">
+            <div className="level">B1</div>
+          </div>
+        </div>
+
+        <div className="services-lane">
+          <div className="level-container">
+            <div className="level">B2</div>
+          </div>
+          <div className="level-container">
+            <div className="level">C1</div>
+          </div>
+          <div className="level-container">
+            <div className="level">C2</div>
+          </div>
+        </div>
+
+        {/* <img
         src="https://cdn.prod.website-files.com/63eca4d199772038e357f01c/65d891a50b03373ebdae0c86_shake%2024.svg"
         alt="SVG"
-      />
+      /> */}
+      </div>
     </div>
   );
 }
