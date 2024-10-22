5b079622d79b6e3d7f065a73ff747a93a8ca6dca
Sercan Barış <sercanbaris@gmail.com>
2024-10-22 22:23:54 +0300

    Update YouTubePlayer CSS styles for responsive design

--- src/pages/YouTubePlayer.jsx
+++ src/pages/YouTubePlayer.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import YouTube from "react-youtube";
 import "../css/YouTubePlayer.css";
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
 
 const YouTubePlayer = ({ playlist }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -18,11 +20,11 @@ const YouTubePlayer = ({ playlist }) => {
   return (
     <div className="youtube-player">
       <button className="prev-button" onClick={handlePrev}>
-        ←
+        <FaArrowLeft />
       </button>
       <YouTube videoId={playlist[currentIndex]} />
       <button className="next-button" onClick={handleNext}>
-        →
+        <FaArrowRight />
       </button>
     </div>
   );
